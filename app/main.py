5f67ddf5d43f4823bd4c9260b9e81696046a82b8
Tejas Goswami <goswamitejas909@gmail.com>
2026-06-23 21:35:06 +0530

    Added production dashboard UI

--- app/main.py
+++ app/main.py
@@ -1,10 +1,50 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Webvory DevOps Assignment Running"}
+@app.get("/", response_class=HTMLResponse)
+def home():
+    return """
+    <html>
+    <head>
+        <title>Webvory DevOps Assignment</title>
+        <style>
+            body {
+                font-family: Arial;
+                background: #0f172a;
+                color: white;
+                text-align: center;
+                padding: 50px;
+            }
+            .card {
+                background: #1e293b;
+                padding: 30px;
+                border-radius: 15px;
+                width: 60%;
+                margin: auto;
+            }
+            .status {
+                color: #22c55e;
+                font-weight: bold;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="card">
+            <h1>🚀 DevOps Production Deployment</h1>
+            <p>Assignment for Webvory</p>
+
+            <h2>Status: <span class="status">Running</span></h2>
+
+            <p>FastAPI + PostgreSQL + Redis + NGINX</p>
+            <p>Dockerized | CI/CD Enabled | Health Checks Active</p>
+
+            <p>Hosted on Azure VPS</p>
+        </div>
+    </body>
+    </html>
+    """
 
 @app.get("/health")
 def health():
